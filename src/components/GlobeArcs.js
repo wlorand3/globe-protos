278bb5983bc111b274cc6f846f1ed64bf5622fde
Will Lorand <wlorand@cisco.com>
2021-07-31 11:06:27 -0700

    feature: added night sky and code to locate the globe at oslo lat/lng

--- src/components/GlobeArcs.js
+++ src/components/GlobeArcs.js
@@ -1,31 +1,45 @@
+import { useEffect, useRef } from "react";
 import Globe from "react-globe.gl";
 
 function GlobeArcs() {
-  // Gen random data
-  const N = 30;
+  const globeEl = useRef();
+
+  // Gen random data, incl lat/longs
+  const N = 20;
   const arcsData = [...Array(N).keys()].map(() => ({
     startLat: (Math.random() - 0.5) * 180,
     startLng: (Math.random() - 0.5) * 360,
     endLat: (Math.random() - 0.5) * 180,
     endLng: (Math.random() - 0.5) * 360,
-    color: [
-      ["red", "white", "blue", "green"][Math.round(Math.random() * 3)],
-      ["red", "white", "blue", "green"][Math.round(Math.random() * 3)],
-    ],
   }));
+
+  useEffect(() => {
+    const mapCenter = { lat: 59.9139, lng: 10.7522, altitude: 2.5 }; // Oslo
+    globeEl.current.pointOfView(mapCenter, 4000);
+  }, []);
+
+  const tooltipHtml = () => {
+    return `<div>
+        <p>White text</p>
+        <p style="color: red">Red text</p>
+      </div>`;
+  };
 
   return (
     <div>
       <Globe
-        // globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+        ref={globeEl}
+        // globeImageUrl="images/land_ocean_ice_cloud_2048.jpg"
         globeImageUrl="/images/earthmap1k.jpg"
+        backgroundImageUrl="/images/night-sky.png"
         arcsData={arcsData}
         arcColor={() => "#00FF00"}
-        arcDashLength={() => Math.random()}
+        arcDashLength={0.3}
         arcDashGap={() => Math.random()}
-        arcDashAnimateTime={2000}
-        arcLabel={() => "swoosh"}
-        arcStroke={1}
+        arcDashAnimateTime={4000}
+        arcAltitudeAutoScale={0.6}
+        arcLabel={() => tooltipHtml()}
+        arcStroke={0.5}
       />
     </div>
   );
